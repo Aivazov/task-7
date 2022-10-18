ee287c1861a3e05810de98fefa7aeb84e147671c
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2022-10-18 13:37:35 +0300

    working on task 1 of module 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const galleryDiv = document.querySelector('.gallery');
 console.log(galleryItems);
+
+renderingGallery(galleryItems);
+
+function renderingGallery(array) {
+  const markup = array
+    .map(
+      (image) => `
+    <div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+        <img
+          class="gallery__image"
+          src="${image.preview}"
+          data-source="large-image.jpg"
+          alt="${image.description}"
+        />
+      </a>
+    </div>`
+    )
+    .join('');
+  galleryDiv.insertAdjacentHTML('beforeend', markup);
+}
--- js/script2.js
+++ js/script2.js
@@ -11,22 +11,57 @@ const filteringBooksTags = booksInfo
   .flatMap((book) => book.tags)
   .filter((tag, index, array) => array.indexOf(tag) === index);
 
+refs.buttonsContainer.addEventListener('click', (event) => {
+  const currentElement = event.target;
+  const currentTarget = event.currentTarget;
+  const currentValue = event.target.dataset.attribute;
+  const activeElement = document.querySelector('.is-active');
+
+  if (!currentValue) return;
+  console.log(currentValue);
+
+  if (activeElement) {
+    activeElement.classList.remove('is-active');
+  }
+
+  if (activeElement === currentElement) {
+    return renderingBooksInfo(booksInfo);
+    // console.log(activeElement === currentElement);
+  }
+
+  currentElement.classList.add('is-active');
+
+  const buttonFilteringTag = booksInfo.filter(book => book.tags.includes(currentValue));
+  // const markup = booksInfo
+  //   .map((listItem) => `<li>${listItem.name}: <b>${listItem.price}</b></li>`)
+  //   .filter((element, index, array) => array.includes(element) === currentValue)
+  //   .join('');
+
+  // refs.listContainer.insertAdjacentHTML('beforeend', markup);
+  // console.log(currentTarget);
+
+  renderingBooksInfo(buttonFilteringTag);
+});
+
 // another way to receive our tags and display it
 // in this case the result is the same
 // const markupUsingSet = [...new Set(booksInfo.flatMap((book) => book.tags))];
 // putting it into [] give the chance to use array's methods
 // console.log(markupUsingSet);
 
+renderingButtons();
+renderingBooksInfo(booksInfo);
+
 function renderingButtons() {
   const markup = filteringBooksTags
-    .map((button) => `<button class="button">${button}</button>`)
+    .map(
+      (button) =>
+        `<button class="button" data-attribute="${button}">${button}</button>`
+    )
     .join('');
 
   refs.buttonsContainer.insertAdjacentHTML('beforeend', markup);
 }
-
-renderingButtons();
-renderingBooksInfo(booksInfo);
 
 function renderingBooksInfo(array) {
   refs.listContainer.innerHTML = '';
@@ -34,6 +69,5 @@ function renderingBooksInfo(array) {
   const markup = array
     .map((listItem) => `<li>${listItem.name}: <b>${listItem.price}</b></li>`)
     .join('');
-  console.log(markup);
   refs.listContainer.insertAdjacentHTML('beforeend', markup);
 }
