c855ccebf4c799473c0b156a8b2828ec7dcf8319
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2022-12-26 10:38:10 +0200

    final commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,113 +1,55 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-// console.log(galleryItems);
-
+import { galleryItems } from './gallery-items-2.js';
+console.log(galleryItems);
 const galleryUl = document.querySelector('.gallery');
+const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+lazyImages.forEach((image) => {
+  image.addEventListener('load', onImageLoaded);
+});
 renderingGallery(galleryItems);
 
-const lazyImages = document.querySelectorAll("img[loading='lazy']");
+let galleryModal = new SimpleLightbox('.gallery__item');
 
-let gallery = new SimpleLightbox('.gallery__item');
-gallery.on('show.simplelightbox');
-
-console.log('gallery.elements', gallery.elements);
-
-// lazyImages.forEach((image) => {
-//   // console.log(image);
-
-//   image.addEventListener("load", onImageLoaded);
-// });
-
-// galleryUl.addEventListener("click", onImageClick);
-
-function onImageClick(e) {
-  e.preventDefault();
-  console.log(e.target.dataset.source);
-  if (e.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  gallery.open();
-
-  // gallery.on("shown.simplelightbox", function () {
-  //   console.log('hi');
-  // });
-
-  // galleryDiv.addEventListener('keydown', (e) => {
-  //   if (e.code === 'Escape') instance.close();
-  // });
-}
-
-function renderingGallery(array) {
-  const markup = array
+galleryModal.options.captionsData = 'alt';
+galleryModal.options.captionDelay = 250;
+galleryModal.on('show.simplelightbox');
+//
+//
+//
+function renderingGallery(galleryArr) {
+  const markup = galleryArr
     .map(
-      (image) =>
-        `
-        <a class="gallery__item" href="${image.original}">
-          <img class="gallery__image" src="${image.preview}" data-source="${image.original}"3 alt="${image.description}" title="${image.description}" loading='lazy' />
+      ({ original, preview, description }) => `
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}"3 alt="${description}" title="${description}" loading='lazy' />
         </a>`
     )
     .join('');
 
   galleryUl.insertAdjacentHTML('afterbegin', markup);
 }
+//
+//
+//
 
-// gallery.open(
-//   'show.simplelightbox',
-//   gallery.elements.forEach((el) => {
-//     el.addEventListener('click', (e) => {
-//       e.preventDefault();
-//       console.log('el', el);
-
-//       if (e.target.nodeName !== 'IMG') {
-//         return;
-//       }
-
-//       let gallery = new SimpleLightbox('.gallery a');
-//       gallery.open();
-
-//       // const galleryImage = document.querySelector('.gallery > a');
-//     });
+// const gallery = galleryItems
+//   .map(({ preview, original, description }) => {
+//     return `<li>
+//         <a class="gallery__item" href=${original}>
+//             <img
+//                 class="gallery__image"
+//                 src=${preview}
+//                 alt=${description}
+//             />
+//         </a>
+//     </li>`;
 //   })
-// );
+//   .join('');
 
-// gallery.elements.forEach((el) => {
-//   el.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     console.log('el', el);
+// const galleryRef = document.querySelector('.gallery');
+// galleryRef.insertAdjacentHTML('afterbegin', gallery);
 
-//     if (e.target.nodeName !== 'IMG') {
-//       return;
-//     }
-//     gallery.on('shown.simplelightbox');
-//     gallery.open();
+// const modal = new SimpleLightbox('.gallery__item');
 
-//     // const galleryImage = document.querySelector('.gallery > a');
-//   });
-// });
-
-// function onImageClick(e) {
-//   e.preventDefault();
-
-//   if (e.target.nodeName !== 'IMG') {
-//     return;
-//   }
-
-//   const galleryImage = document.querySelector('.gallery > a');
-//   // gallery.open();
-//   gallery.destroy();
-
-//   // galleryImage.addEventListener('click', () => {
-//   // });
-
-//   //   const instance = basicLightbox.create(`
-//   //     <img src="${e.target.dataset.source}" width="800" height="600">
-//   // `);
-
-//   // e.target.src.open();
-// }
-
-function onImageLoaded(e) {
-  // console.log(e.target);
-}
+// modal.options.captionsData = 'alt';
+// modal.options.captionDelay = 250;
+// modal.on('show.simplelightbox');
---
+++ js/gallery-items-2.js
@@ -0,0 +1,65 @@
+export const galleryItems = [
+  {
+    preview:
+      'https://images.pexels.com/photos/3422964/pexels-photo-3422964.jpeg?auto=compress&cs=tinysrgb&w=340',
+    original:
+      'https://images.pexels.com/photos/3422964/pexels-photo-3422964.jpeg',
+    description: 'Lamborghini Aventador',
+  },
+  {
+    preview:
+      'https://images.pexels.com/photos/2356071/pexels-photo-2356071.jpeg?auto=compress&cs=tinysrgb&w=340',
+    original:
+      'https://images.pexels.com/photos/2356071/pexels-photo-2356071.jpeg',
+    description: 'Mountain Sight',
+  },
+  {
+    preview:
+      'https://images.pexels.com/photos/1834403/pexels-photo-1834403.jpeg?auto=compress&cs=tinysrgb&w=340',
+    original:
+      'https://images.pexels.com/photos/1834403/pexels-photo-1834403.jpeg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://images.pexels.com/photos/36487/above-adventure-aerial-air.jpg?auto=compress&cs=tinysrgb&w=340',
+    original:
+      'https://images.pexels.com/photos/36487/above-adventure-aerial-air.jpg',
+    description: 'Baloon to the Horizon',
+  },
+  {
+    preview:
+      'https://images.pexels.com/photos/52500/horse-herd-fog-nature-52500.jpeg?auto=compress&cs=tinysrgb&w=340',
+    original:
+      'https://images.pexels.com/photos/52500/horse-herd-fog-nature-52500.jpeg',
+    description: 'Irish Pasturates Horses',
+  },
+  {
+    preview:
+      'https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?auto=compress&cs=tinysrgb&w=340',
+    original:
+      'https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://images.pexels.com/photos/1379636/pexels-photo-1379636.jpeg?auto=compress&cs=tinysrgb&w=340',
+    original:
+      'https://images.pexels.com/photos/1379636/pexels-photo-1379636.jpeg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://images.pexels.com/photos/4338020/pexels-photo-4338020.jpeg?auto=compress&cs=tinysrgb&w=340',
+    original:
+      'https://images.pexels.com/photos/4338020/pexels-photo-4338020.jpeg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://images.pexels.com/photos/785293/pexels-photo-785293.jpeg?auto=compress&cs=tinysrgb&w=340',
+    original:
+      'https://images.pexels.com/photos/785293/pexels-photo-785293.jpeg',
+    description: 'Flowers Blossom',
+  },
+];
